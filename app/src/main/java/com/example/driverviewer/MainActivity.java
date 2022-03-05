66c59b1b8bb7d3bf42b63aa55b2c760b38754cb1
Asım <44242040+asimcaliskan@users.noreply.github.com>
2022-03-05 15:36:06 +0300

    stream added

--- app/src/main/java/com/example/driverviewer/MainActivity.java
+++ app/src/main/java/com/example/driverviewer/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.driverviewer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MULTIPLE_PERMISSIONS = 958;
+    private final String[] permissions = new String[]{
+            Manifest.permission.CAMERA,
+            Manifest.permission.INTERNET,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermissions();
+        Button streamButton = findViewById(R.id.button_stream);
+        streamButton.setOnClickListener(view -> {
+            Intent changeActivityIntent = new Intent(MainActivity.this, StreamActivity.class);
+            MainActivity.this.startActivity(changeActivityIntent);
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+/*
+        switch (requestCode) {
+            case MULTIPLE_PERMISSIONS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //getCallDetails(); // Now you call here what ever you want :)
+                } else {
+                    String perStr = "";
+                    for (String per : permissions) {
+                        perStr += "\n" + per;
+                    }   // permissions list of don't granted permission
+                }
+                return;
+            }
+        }
+*/
+    }
+
+    private boolean checkPermissions() {
+        int result;
+        List<String> listPermissionsNeeded = new ArrayList<>();
+
+        for (String p : permissions) {
+            result = ContextCompat.checkSelfPermission(getApplicationContext(), p);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(p);
+            }
+        }
+
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[0]), MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/driverviewer/StreamActivity.java
@@ -0,0 +1,140 @@
+package com.example.driverviewer;
+
+import android.graphics.Bitmap;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Size;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+
+public class StreamActivity extends AppCompatActivity {
+    private ListenableFuture<ProcessCameraProvider> cameraProviderListenableFuture;
+    private PreviewView streamPreviewView = null;
+    private Button startStopStreamButton = null;
+    private Boolean isStreaming = false;
+    private Bitmap bitmap = null;
+    private RequestQueue queue = null;
+    private String URL = "http://192.168.1.52:5000/authenticate/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stream);
+
+        streamPreviewView = findViewById(R.id.preview_view_stream);
+        streamPreviewView.setScaleType(PreviewView.ScaleType.FILL_CENTER);
+
+        startStopStreamButton = findViewById(R.id.button_start_stop_stream);
+        startStopStreamButton.setOnClickListener(view -> {
+            if (isStreaming) {
+                startStopStreamButton.setText(R.string.start_stream);
+            } else {
+                startStopStreamButton.setText(R.string.stop_stream);
+            }
+            isStreaming = !isStreaming;
+        });
+
+        queue = Volley.newRequestQueue(this);
+
+        cameraProviderListenableFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderListenableFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider processCameraProvider = cameraProviderListenableFuture.get();
+                bindPreview(processCameraProvider);
+            } catch (ExecutionException | InterruptedException exception) {
+                toastMaker("Error in cameraProviderListenableFuture.addListener" + exception);
+            }
+        }, ContextCompat.getMainExecutor(this));
+
+
+    }
+
+    private void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder()
+                .build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
+                .build();
+
+        preview.setSurfaceProvider(streamPreviewView.getSurfaceProvider());
+
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview);
+
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                .setTargetResolution(new Size(640, 480))
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build();
+
+
+        imageAnalysis.setAnalyzer(Executors.newSingleThreadExecutor(), imageProxy -> {
+            if (imageProxy.getFormat() == PixelFormat.RGBA_8888) {
+                if (bitmap == null) {
+                    bitmap = Bitmap.createBitmap(
+                            imageProxy.getWidth(),
+                            imageProxy.getHeight(),
+                            Bitmap.Config.ARGB_8888
+                    );
+                }
+                bitmap.copyPixelsFromBuffer(imageProxy.getPlanes()[0].getBuffer());
+                if (isStreaming) {
+                    try {
+                        post();
+                    } catch (JSONException e) {
+                        toastMaker(e.toString());
+                        e.printStackTrace();
+                    }
+                }
+            }
+            imageProxy.close();
+        });
+        cameraProvider.bindToLifecycle(this, cameraSelector, imageAnalysis, preview);
+    }
+
+    private void post() throws JSONException {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 0, stream);
+        byte[] byteArray = stream.toByteArray();
+        String imageString = Base64.encodeToString(byteArray, Base64.DEFAULT);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.putOpt("image", imageString);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, URL, jsonObject, response -> {
+            toastMaker("SUCCESS");
+        }, error -> {
+            toastMaker("ERROR " + error);
+        });
+        queue.add(jsonObjectRequest);
+    }
+
+    private void toastMaker(String text){
+        //Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+    }
+}
