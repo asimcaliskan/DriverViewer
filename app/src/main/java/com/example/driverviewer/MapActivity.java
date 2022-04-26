24dafcee0a168f02774ee9da725517bdb98591a2
Asım <44242040+asimcaliskan@users.noreply.github.com>
2022-04-26 13:03:43 +0300

    map added

--- app/src/main/java/com/example/driverviewer/MapActivity.java
+++ app/src/main/java/com/example/driverviewer/MapActivity.java
@@ -5,9 +5,29 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker About Something"));
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(1, 1))
+                .title("Marker About Another Thing"));
     }
 }
